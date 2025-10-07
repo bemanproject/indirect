11dcebcc23720ef963b5286c372dc1cd42070a99
wusatosi <26424577+wusatosi@users.noreply.github.com>
2025-10-07 19:05:41 +0000

    Reinit from beman exemplar

---
+++ include/beman/indirect/indirect.hpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef BEMAN_INDIRECT_IDENTITY_HPP
+#define BEMAN_INDIRECT_IDENTITY_HPP
+
+namespace beman::indirect {
+
+class Indirect;
+
+} // namespace beman::indirect
+
+#endif // BEMAN_INDIRECT_IDENTITY_HPP
---
+++ tests/beman/indirect/indirect.test.cpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <beman/indirect/indirect.hpp>
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <functional>
+
+namespace exe = beman::indirect;
+
+TEST(IndirectTests, EnvSetup) {}
