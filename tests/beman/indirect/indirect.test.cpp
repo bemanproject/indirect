02103b21e44b1f0cd0d77dfd2f0376f33a67f60e
wusatosi <26424577+wusatosi@users.noreply.github.com>
2025-10-20 06:20:31 +0000

    added tests to constructors

--- tests/beman/indirect/indirect.test.cpp
+++ tests/beman/indirect/indirect.test.cpp
@@ -15,7 +15,7 @@ struct CountingAllocator {
     using Id_type         = int;
 
     constexpr CountingAllocator() = default;
-    constexpr explicit CountingAllocator(Id_type init_num) : id(init_num) {}
+    constexpr explicit CountingAllocator(Id_type _id) : id(_id) {}
     constexpr ~CountingAllocator() = default;
 
     constexpr T* allocate(std::size_t n) {
@@ -42,19 +42,64 @@ static_assert(std::invoke([]() {
     return true;
 }));
 
+#define ASSERT_NO_LEAKS(alloc) EXPECT_EQ(alloc.num_allocated, alloc.num_deallocated)
+
 template <typename T>
 using indirect = beman::indirect::indirect<T, CountingAllocator<T>>;
 
-struct Incomplete {
-    using Self = Incomplete;
+// TODO: Move these to a header file and add factory methods so they can be in another TU to test for incomplete type.
+struct Composite {
+    int a = 0;
+    int b = 0;
+    int c = 0;
 
-    indirect<Self> ind;
-    int            _ignore;
+    Composite() = default;
+    Composite(int _a, int _b, int _c) : a(_a), b(_b), c(_c) {}
+
+    bool operator==(const Composite& other) const { return a == other.a && b == other.b && c == other.c; }
 };
 
-auto ASSERT_ALLOC_EVEN(const auto& alloc) { ASSERT_EQ(alloc.num_allocated, alloc.num_deallocated); }
+struct SimpleType {
+    int value;
+    explicit SimpleType(int v) : value(v) {}
+    bool operator==(const SimpleType& other) const { return value == other.value; }
+};
 
-TEST(IndirectRawTest, CanHoldIncompleteType) {
+struct MoveOnlyType {
+    int value;
+    explicit MoveOnlyType(int v) : value(v) {}
+    MoveOnlyType(const MoveOnlyType&) = delete;
+    MoveOnlyType(MoveOnlyType&& other) noexcept : value(other.value) { other.value = -1; }
+    MoveOnlyType& operator=(const MoveOnlyType&) = delete;
+    MoveOnlyType& operator=(MoveOnlyType&&)      = delete;
+    bool          operator==(const MoveOnlyType& other) const { return value == other.value; }
+};
+
+struct DefaultConstructible {
+    int  value = 55;
+    bool operator==(const DefaultConstructible& other) const { return value == other.value; }
+};
+
+struct VectorLike {
+    std::vector<int> data;
+    explicit VectorLike(std::initializer_list<int> ilist) : data(ilist) {}
+    bool operator==(const VectorLike& other) const { return data == other.data; }
+};
+
+struct VectorWithMultiplier {
+    std::vector<int> data;
+    int              multiplier;
+    explicit VectorWithMultiplier(std::initializer_list<int> ilist, int mult) : data(ilist), multiplier(mult) {}
+    bool operator==(const VectorWithMultiplier& other) const {
+        return data == other.data && multiplier == other.multiplier;
+    }
+};
+
+// ========================================
+// Incomplete Type Tests
+// ========================================
+
+TEST(IncompleteTests, CanHoldIncompleteType) {
     // Passes if it compiles, this is statuary
 
     struct Incomplete {
@@ -65,69 +110,254 @@ TEST(IndirectRawTest, CanHoldIncompleteType) {
     };
 }
 
-// Should be generic
-TEST(IncompleteTests, DefaultInitializable) { indirect<Incomplete> instance; }
+// ========================================
+// Default Constructor Tests
+// ========================================
 
-TEST(IncompleteTests, DefaultInitializableWAllocator) {
-    CountingAllocator<Incomplete> alloc(20);
-    {
-        indirect<Incomplete> instance(std::allocator_arg_t{}, alloc);
-    }
-    ASSERT_ALLOC_EVEN(alloc);
-    // Make sure the allocator is actually passed in
-    ASSERT_EQ(alloc.id, 20);
+TEST(IndirectTest, DefaultConstructor) {
+    using T = DefaultConstructible;
+    indirect<T> instance;
+
+    EXPECT_FALSE(instance.valueless_after_move());
 }
 
-template <std::size_t* num_constructor_calls>
-struct Composite {
-    Composite() { (*num_constructor_calls)++; }
-    Composite(int _a, int _b, int _c) : a(_a), b(_b), c(_c) { (*num_constructor_calls)++; }
+TEST(IndirectTest, DefaultConstructorWithAllocator) {
+    using T = DefaultConstructible;
+    CountingAllocator<T> alloc;
 
-    int a, b, c;
-};
+    {
+        indirect<T> instance(std::allocator_arg, alloc);
 
-TEST(IndirectTest, InitializeWForward) {
-    static std::size_t num_constructor_calls = 0;
-    num_constructor_calls                    = 0;
+        EXPECT_FALSE(instance.valueless_after_move());
+    }
 
-    using T = Composite<&num_constructor_calls>;
-    indirect<T> instance(1, 2, 3);
+    ASSERT_NO_LEAKS(alloc);
+    EXPECT_EQ(alloc.num_allocated, 1);
+}
 
-    EXPECT_EQ(num_constructor_calls, 1);
+// ========================================
+// in_place Constructor Tests
+// ========================================
+
+TEST(IndirectTest, InPlaceConstructorBasic) {
+    using T = Composite;
+    indirect<T> instance(std::in_place, 1, 2, 3);
+
     EXPECT_EQ(*instance, T(1, 2, 3));
 }
 
-TEST(IndirectTest, InitializeWForwardAndAlloc) {
-    static std::size_t num_constructor_calls = 0;
-    num_constructor_calls                    = 0;
-
-    using T = Composite<&num_constructor_calls>;
+TEST(IndirectTest, InPlaceConstructorBasicWithAllocator) {
+    using T = Composite;
     CountingAllocator<T> alloc(25);
 
     {
-        indirect<T> instance(std::allocator_arg_t{}, alloc, 1, 2, 3);
-        EXPECT_EQ(num_constructor_calls, 1);
+        indirect<T> instance(std::allocator_arg, alloc, std::in_place, 1, 2, 3);
+
         EXPECT_EQ(*instance, T(1, 2, 3));
     }
 
-    ASSERT_ALLOC_EVEN(alloc);
-    EXPECT_EQ(alloc.id, 25);
+    ASSERT_NO_LEAKS(alloc);
+    EXPECT_EQ(alloc.num_allocated, 1);
 }
 
-TEST(IncompleteTests, InitializeWForwardAndAlloc) {
-    static std::size_t num_constructor_calls = 0;
-    num_constructor_calls                    = 0;
+TEST(IndirectTest, InPlaceConstructorNoArgs) {
+    using T = DefaultConstructible;
+    indirect<T> instance(std::in_place);
 
+    EXPECT_EQ(*instance, T());
+}
+
+TEST(IndirectTest, InPlaceConstructorWithArgs) {
+    using T = Composite;
+    indirect<T> instance(std::in_place, 5, 10, 15);
+
+    EXPECT_EQ(*instance, T(5, 10, 15));
+}
+
+TEST(IndirectTest, InPlaceConstructorWithAllocator) {
+    using T = Composite;
+    CountingAllocator<T> alloc(400);
+
+    {
+        indirect<T> instance(std::allocator_arg, alloc, std::in_place, 7, 8, 9);
+
+        EXPECT_EQ(*instance, T(7, 8, 9));
+    }
+
+    ASSERT_NO_LEAKS(alloc);
+}
+
+TEST(IncompleteTests, InPlaceConstructorWithIncompleteType) {
     struct T;
     CountingAllocator<T> alloc(25);
 
     struct T {
         int         a, b, c;
-        indirect<T> ind{std::allocator_arg_t{}, alloc, 1, 2, 3};
+        indirect<T> ind{std::allocator_arg, alloc, std::in_place, 1, 2, 3};
     };
 
     indirect<T> instance;
 
-    ASSERT_ALLOC_EVEN(alloc);
+    ASSERT_NO_LEAKS(alloc);
     EXPECT_EQ(alloc.num_allocated, 26);
+}
+
+// ========================================
+// Copy Constructor Tests
+// ========================================
+
+TEST(IndirectTest, CopyConstructor) {
+    using T = Composite;
+    indirect<T> original(std::in_place, 10, 20, 30);
+
+    indirect<T> copy(original);
+
+    EXPECT_EQ(*copy, T(10, 20, 30));
+    EXPECT_EQ(*original, T(10, 20, 30));
+
+    // Verify copy is independent - modify copy and check original unchanged
+    copy->a = 999;
+    EXPECT_EQ(*copy, T(999, 20, 30));
+    EXPECT_EQ(*original, T(10, 20, 30));
+}
+
+TEST(IndirectTest, CopyConstructorWithAllocator) {
+    using T = Composite;
+    CountingAllocator<T> alloc1(100);
+    CountingAllocator<T> alloc2(200);
+
+    {
+        indirect<T> original(std::allocator_arg, alloc1, std::in_place, 10, 20, 30);
+
+        indirect<T> copy(std::allocator_arg, alloc2, original);
+
+        EXPECT_EQ(*copy, T(10, 20, 30));
+
+        // Verify copy is independent
+        copy->b = 888;
+        EXPECT_EQ(*copy, T(10, 888, 30));
+        EXPECT_EQ(*original, T(10, 20, 30));
+    }
+
+    ASSERT_NO_LEAKS(alloc1);
+    ASSERT_NO_LEAKS(alloc2);
+}
+
+// ========================================
+// Move Constructor Tests
+// ========================================
+
+TEST(IndirectTest, MoveConstructor) {
+    using T = Composite;
+    indirect<T> original(std::in_place, 10, 20, 30);
+
+    EXPECT_FALSE(original.valueless_after_move());
+
+    indirect<T> moved(std::move(original));
+
+    EXPECT_EQ(*moved, T(10, 20, 30));
+    EXPECT_TRUE(original.valueless_after_move());
+}
+
+TEST(IndirectTest, MoveConstructorWithAllocatorSameAllocator) {
+    using T = Composite;
+    CountingAllocator<T> alloc(100);
+
+    {
+        indirect<T> original(std::allocator_arg, alloc, std::in_place, 10, 20, 30);
+        EXPECT_FALSE(original.valueless_after_move());
+
+        indirect<T> moved(std::allocator_arg, alloc, std::move(original));
+
+        EXPECT_EQ(*moved, T(10, 20, 30));
+        EXPECT_TRUE(original.valueless_after_move());
+    }
+
+    ASSERT_NO_LEAKS(alloc);
+}
+
+// ========================================
+// Forwarding Constructor Tests (U&&)
+// ========================================
+
+TEST(IndirectTest, ForwardingConstructorFromLValue) {
+    using T = SimpleType;
+    T           value(42);
+    indirect<T> instance(value);
+
+    EXPECT_EQ(*instance, T(42));
+    EXPECT_EQ(value, T(42)); // Original should be unchanged
+}
+
+TEST(IndirectTest, ForwardingConstructorFromRValue) {
+    using T = SimpleType;
+    indirect<T> instance(T(42));
+
+    EXPECT_EQ(*instance, T(42));
+}
+
+TEST(IndirectTest, ForwardingConstructorFromLValueWithAllocator) {
+    using T = SimpleType;
+    CountingAllocator<T> alloc(300);
+
+    {
+        T           value(42);
+        indirect<T> instance(std::allocator_arg, alloc, value);
+
+        EXPECT_EQ(*instance, T(42));
+        EXPECT_EQ(value, T(42)); // Original should be unchanged
+    }
+
+    ASSERT_NO_LEAKS(alloc);
+}
+
+TEST(IndirectTest, ForwardingConstructorFromMoveOnlyType) {
+    using T = MoveOnlyType;
+    indirect<T> instance(T(99));
+
+    EXPECT_EQ(*instance, T(99));
+}
+
+// ========================================
+// initializer_list Constructor Tests
+// ========================================
+
+TEST(IndirectTest, InitializerListConstructor) {
+    using T = VectorLike;
+    indirect<T> instance(std::in_place, {1, 2, 3, 4, 5});
+
+    EXPECT_EQ(*instance, T({1, 2, 3, 4, 5}));
+}
+
+TEST(IndirectTest, InitializerListConstructorWithArgs) {
+    using T = VectorWithMultiplier;
+    indirect<T> instance(std::in_place, {10, 20, 30}, 2);
+
+    EXPECT_EQ(*instance, T({10, 20, 30}, 2));
+}
+
+TEST(IndirectTest, InitializerListConstructorWithAllocator) {
+    using T = VectorLike;
+    CountingAllocator<T> alloc(500);
+
+    {
+        indirect<T> instance(std::allocator_arg, alloc, std::in_place, {7, 8, 9});
+
+        EXPECT_EQ(*instance, T({7, 8, 9}));
+    }
+
+    ASSERT_NO_LEAKS(alloc);
+}
+
+TEST(IndirectTest, InitializerListConstructorWithAllocatorAndArgs) {
+    using T = VectorWithMultiplier;
+    CountingAllocator<T> alloc(600);
+
+    {
+        indirect<T> instance(std::allocator_arg, alloc, std::in_place, {100, 200}, 5);
+
+        EXPECT_EQ(*instance, T({100, 200}, 5));
+    }
+
+    ASSERT_NO_LEAKS(alloc);
 }
